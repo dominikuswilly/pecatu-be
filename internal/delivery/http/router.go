3effd25622ff06bf01311e50f57f957870f10856
willy willy <medicalawn@gmail.com>
2026-06-13 22:22:53 +0700

    [26061301] add public

--- internal/delivery/http/router.go
+++ internal/delivery/http/router.go
@@ -8,7 +8,7 @@ func NewRouter(healthHandler *HealthHandler, donateHandler *DonateHandler) *http
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /health", healthHandler.Check)
-	mux.HandleFunc("GET /donate/{id}", donateHandler.GetByID)
+	mux.HandleFunc("GET /public/donate/{id}", donateHandler.GetByID)
 
 	return mux
 }
